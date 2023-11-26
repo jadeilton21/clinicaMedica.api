42bf4b53cf8337b84c4da1dc6c4e878207ff9eed
jadeilton <jadeiltondm@gmail.com>
2023-11-26 19:34:50 -0300

    Initial commit

---
+++ src/main/java/clinicaMedica/api/clinicaMeidica/api/Application.java
@@ -0,0 +1,13 @@
+package clinicaMedica.api.clinicaMeidica.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
