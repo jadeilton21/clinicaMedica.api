e7c024fe3073fa3c003eb79eaf81caf9effda41c
jadeilton <jadeiltondm@gmail.com>
2023-11-27 07:31:39 -0300

    feat : create class Medico

---
+++ src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/medico/Medico.java
@@ -0,0 +1,44 @@
+package clinicaMedica.api.clinicaMeidica.api.doMain.medico;
+
+
+import clinicaMedica.api.clinicaMeidica.api.doMain.endereco.Endereco;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "medicos")
+@Entity(name = "Medico")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Medico {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+
+    private String telefone;
+
+    private String crm;
+    @Enumerated(EnumType.STRING)
+    private Especialidade especialidade;
+    @Embedded
+    private Endereco endereco;
+
+    private Boolean ativo;
+
+    public Medico(DadosCadastrarMedico dados) {
+
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+        this.crm = crm;
+        this.especialidade = especialidade;
+        this.endereco = endereco;
+        this.ativo = true;
+    }
+}
