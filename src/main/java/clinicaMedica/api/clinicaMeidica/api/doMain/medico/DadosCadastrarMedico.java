146cb1429be2160dfdd376f491cd779c78bcdae7
jadeilton <jadeiltondm@gmail.com>
2023-11-27 11:06:14 -0300

    feat : update class DadosCadastrarMedico

--- src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/medico/DadosCadastrarMedico.java
+++ src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/medico/DadosCadastrarMedico.java
@@ -1,4 +1,25 @@
 package clinicaMedica.api.clinicaMeidica.api.doMain.medico;
 
-public record DadosCadastrarMedico() {
+import clinicaMedica.api.clinicaMeidica.api.doMain.endereco.DadosEndereco;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
+public record DadosCadastrarMedico(
+        @NotBlank
+        String nome,
+        @NotBlank
+        @Email
+        String email,
+        @NotBlank
+        String telefone,
+        @NotBlank
+        @Pattern(regexp = "\\d{4,6}")
+        String crm,
+        @NotNull
+        Especialidade especialidade,
+        @Valid @NotNull DadosEndereco dadosEndereco
+) {
 }
