ea11afc2cd8498c8f93b9d899955f277c5e76c38
jadeilton <jadeiltondm@gmail.com>
2023-11-29 14:42:47 -0300

    feat : udapte class Enfermeiro

--- src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/enfermeiro/Enfermeiro.java
+++ src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/enfermeiro/Enfermeiro.java
@@ -1,4 +1,43 @@
 package clinicaMedica.api.clinicaMeidica.api.doMain.enfermeiro;
 
+import clinicaMedica.api.clinicaMeidica.api.doMain.endereco.Endereco;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "enfermeiros")
+@Entity(name = "enfermeiro")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Enfermeiro {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String cpf;
+
+    private String telefone;
+
+    private Boolean ativo;
+    private String coren;
+    @Embedded
+    private Endereco endereco;
+    @Enumerated(EnumType.STRING)
+    private EspecialidadeDoEnfermeiro especialidadeDoEnfermeiro;
+
+    public Enfermeiro(DadosCadastrarEnfermeiro dados) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.ativo = ativo;
+        this.coren = coren;
+        this.endereco = endereco;
+        this.especialidadeDoEnfermeiro = especialidadeDoEnfermeiro;
+    }
 }
