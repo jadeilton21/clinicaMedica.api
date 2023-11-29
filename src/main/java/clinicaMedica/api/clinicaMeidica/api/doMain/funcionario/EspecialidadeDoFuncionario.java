bc5b75c63077b86caf71605aac4699a8dd543577
jadeilton <jadeiltondm@gmail.com>
2023-11-29 10:48:27 -0300

    feat : Especialidades descritas

--- src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/funcionario/EspecialidadeDoFuncionario.java
+++ src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/funcionario/EspecialidadeDoFuncionario.java
@@ -3,7 +3,11 @@ package clinicaMedica.api.clinicaMeidica.api.doMain.funcionario;
 public enum EspecialidadeDoFuncionario {
 
 
-
+    CONZINHAR,
+    FAXINAR,
+    ATENDENTE,
+    TESOURARIA,
+    SECREATIRIA
 
 
 
