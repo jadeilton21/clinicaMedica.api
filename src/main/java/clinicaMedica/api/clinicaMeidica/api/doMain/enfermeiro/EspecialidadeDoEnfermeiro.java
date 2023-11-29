a5b25d1fb5e270552ae9f4ea5643bb8356a803d7
jadeilton <jadeiltondm@gmail.com>
2023-11-29 14:43:03 -0300

    feat : Create enum EspecialidadeDoEnfermeiro

---
+++ src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/enfermeiro/EspecialidadeDoEnfermeiro.java
@@ -0,0 +1,10 @@
+package clinicaMedica.api.clinicaMeidica.api.doMain.enfermeiro;
+
+public enum EspecialidadeDoEnfermeiro {
+
+    ENFERMEIRO_CHEFE,
+    ENFERMEIRO_ALA_VERMELHA,
+    ENFERMEIRO_CLINICO,
+    ENFERMEIRO_GERAL
+
+}
