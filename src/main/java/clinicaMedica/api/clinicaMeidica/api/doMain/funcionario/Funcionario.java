e7bffc391ab9cf61a6a0ac44f1be27f8f6a58ac9
jadeilton <jadeiltondm@gmail.com>
2023-11-29 14:43:46 -0300

    feat : update class Funcionario

--- src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/funcionario/Funcionario.java
+++ src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/funcionario/Funcionario.java
@@ -1,4 +1,40 @@
 package clinicaMedica.api.clinicaMeidica.api.doMain.funcionario;
 
+import clinicaMedica.api.clinicaMeidica.api.doMain.endereco.Endereco;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "funcionarios")
+@Entity(name = "funcionario")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Funcionario {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String cpf;
+    private String email;
+
+    private String telefone;
+
+    private Boolean ativo;
+    @Enumerated(EnumType.STRING)
+    private EspecialidadeDoFuncionario especialidadeDoFuncionario;
+    @Embedded
+    private Endereco endereco;
+
+    public Funcionario(DadosCadastrarFuncionario dados) {
+        this.nome = dados.nome();
+        this.cpf = dados.cpf();
+        this.email = dados.email();
+        this.telefone = dados.telefone();
+        this.ativo = true;
+        this.especialidadeDoFuncionario = dados.especialidadeDoFuncionario();
+        this.endereco = new Endereco(dados.dadosEndereco());
+    }
 }
