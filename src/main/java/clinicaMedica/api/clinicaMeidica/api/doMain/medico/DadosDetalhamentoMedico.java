27dcb650b4c6f5c946f7134fcb20ab4fd8737c53
jadeilton <jadeiltondm@gmail.com>
2023-11-29 09:10:17 -0300

    feat : create record DadosDetalhamentoMedico

--- src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/medico/DadosDetalhamentoMedico.java
+++ src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/medico/DadosDetalhamentoMedico.java
@@ -3,4 +3,11 @@ package clinicaMedica.api.clinicaMeidica.api.doMain.medico;
 import clinicaMedica.api.clinicaMeidica.api.doMain.endereco.Endereco;
 
 public record DadosDetalhamentoMedico(Long id, String nome, String email, String crm, String telefone, Especialidade especialidade, Endereco endereco) {
+
+
+    public DadosDetalhamentoMedico (Medico medico){
+
+        this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getTelefone(), medico.getEspecialidade(), medico.getEndereco());
+
+    }
 }
