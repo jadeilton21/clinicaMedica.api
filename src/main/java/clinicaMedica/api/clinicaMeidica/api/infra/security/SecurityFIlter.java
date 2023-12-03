9e2316f230c2db848acb36e979d3291dd9029148
jadeilton <jadeiltondm@gmail.com>
2023-12-03 13:29:13 -0300

    feat: update class SecurityFIlter

--- src/main/java/clinicaMedica/api/clinicaMeidica/api/infra/security/SecurityFIlter.java
+++ src/main/java/clinicaMedica/api/clinicaMeidica/api/infra/security/SecurityFIlter.java
@@ -1,6 +1,8 @@
 package clinicaMedica.api.clinicaMeidica.api.infra.security;
 
+import clinicaMedica.api.clinicaMeidica.api.doMain.usuario.UsuarioRepository;
 import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,8 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
 
 @Component
 public class SecurityFIlter  extends OncePerRequestFilter {
