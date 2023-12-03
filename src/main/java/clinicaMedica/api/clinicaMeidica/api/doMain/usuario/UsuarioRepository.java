bc4bbce2f91260ab5cc0d63b27a241089848bbc4
jadeilton <jadeiltondm@gmail.com>
2023-12-03 13:44:44 -0300

    feat: create Interface UsuarioRepository

---
+++ src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/usuario/UsuarioRepository.java
@@ -0,0 +1,8 @@
+package clinicaMedica.api.clinicaMeidica.api.doMain.usuario;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
+    UserDetails findByLogin(String username);
+}
