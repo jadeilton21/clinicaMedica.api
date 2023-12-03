66625c6178ac95861cfbe3e0ab167db5bc80c998
jadeilton <jadeiltondm@gmail.com>
2023-12-03 13:45:01 -0300

    feat: create class ValidacaoException

---
+++ src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/ValidacaoException.java
@@ -0,0 +1,9 @@
+package clinicaMedica.api.clinicaMeidica.api.doMain;
+
+
+
+public class ValidacaoException extends RuntimeException {
+    public ValidacaoException(String mensagem) {
+        super(mensagem);
+    }
+}
