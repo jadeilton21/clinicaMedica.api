92b2a4a59e569202ca804e3a67f8d5e1488837c3
jadeilton <jadeiltondm@gmail.com>
2023-12-04 16:43:02 -0300

    feat: create method atualizacaoMedico in class Medico

--- src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/medico/Medico.java
+++ src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/medico/Medico.java
@@ -43,4 +43,15 @@ public class Medico {
         this.ativo = true;
     }
 
+    public void atualizarInformacoes(DadosAtualizacaoMedicos dados) {
+        if (dados.nome() != null) {
+            this.nome = dados.nome();
+        }
+        if (dados.telefone() != null) {
+            this.telefone = dados.telefone();
+        }
+        if (dados.endereco() != null) {
+            this.endereco.atualizarInformacoes(dados.endereco());
+        }
+    }
 }
