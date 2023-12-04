ca01f4b902331c05efd06fcd40d5e64a3dca2e75
jadeilton <jadeiltondm@gmail.com>
2023-12-04 10:05:12 -0300

    feat: update Interface MedicoRepository

--- src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/medico/MedicoRepository.java
+++ src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/medico/MedicoRepository.java
@@ -1,6 +1,13 @@
 package clinicaMedica.api.clinicaMeidica.api.doMain.medico;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDateTime;
 
 public interface MedicoRepository extends JpaRepository<Medico, Long> {
+    Page<Medico> findAllByAtivoTrue(Pageable paginacao);
+
 }
