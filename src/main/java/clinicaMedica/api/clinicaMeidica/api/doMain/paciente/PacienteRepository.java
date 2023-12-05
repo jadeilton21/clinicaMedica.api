6f8c78716653605bbbbff3bfeb25dfda7efafb5d
jadeilton <jadeiltondm@gmail.com>
2023-12-05 20:41:22 -0300

    feat: create record PacienteController

--- src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/paciente/PacienteRepository.java
+++ src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/paciente/PacienteRepository.java
@@ -1,6 +1,13 @@
 package clinicaMedica.api.clinicaMeidica.api.doMain.paciente;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.awt.print.Pageable;
+import java.util.Optional;
+
+
 public interface PacienteRepository extends JpaRepository<Paciente,Long> {
+    Page<Paciente> findAllByAtivoTrue(Pageable paginacao);
+
 }
