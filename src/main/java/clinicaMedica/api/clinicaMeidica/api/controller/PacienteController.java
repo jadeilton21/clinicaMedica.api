6847ba15ecaa284a227bd201d1d62444228b3faa
jadeilton <jadeiltondm@gmail.com>
2023-12-05 18:58:37 -0300

    feat: update class PacienteController

--- src/main/java/clinicaMedica/api/clinicaMeidica/api/controller/PacienteController.java
+++ src/main/java/clinicaMedica/api/clinicaMeidica/api/controller/PacienteController.java
@@ -1,8 +1,15 @@
 package clinicaMedica.api.clinicaMeidica.api.controller;
 
 
+import clinicaMedica.api.clinicaMeidica.api.doMain.paciente.DadosCadastrarPaciente;
 import clinicaMedica.api.clinicaMeidica.api.doMain.paciente.PacienteRepository;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +19,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class PacienteController {
 
 
-
+    @Autowired
     private PacienteRepository pacienteRepository;
+
+    @PostMapping
+    @Transactional
+    public void cadastrarPaciente (@RequestBody @Valid DadosCadastrarPaciente dadosCadastrarPaciente){
+
+    }
+
+
 }
