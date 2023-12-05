8ddfa48c4bceaecb5cc2e01ac5b276d0314066ec
jadeilton <jadeiltondm@gmail.com>
2023-12-05 09:46:09 -0300

    feat: update class PacienteController

--- src/main/java/clinicaMedica/api/clinicaMeidica/api/controller/PacienteController.java
+++ src/main/java/clinicaMedica/api/clinicaMeidica/api/controller/PacienteController.java
@@ -1,4 +1,17 @@
 package clinicaMedica.api.clinicaMeidica.api.controller;
 
+
+import clinicaMedica.api.clinicaMeidica.api.doMain.paciente.PacienteRepository;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("pacientes")
+@SecurityRequirement(name = "bearer-key")
 public class PacienteController {
+
+
+
+    private PacienteRepository pacienteRepository;
 }
