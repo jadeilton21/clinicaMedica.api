df4665f30d443f96f3b17b4fffb6c638693aef6e
jadeilton <jadeiltondm@gmail.com>
2023-12-05 08:17:22 -0300

    feat: update class Paciente

--- src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/paciente/Paciente.java
+++ src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/paciente/Paciente.java
@@ -1,4 +1,35 @@
 package clinicaMedica.api.clinicaMeidica.api.doMain.paciente;
 
+import clinicaMedica.api.clinicaMeidica.api.doMain.endereco.Endereco;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity(name="Paciente")
+@Table(name = "pacientes")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Paciente {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String cpf;
+    private String telefone;
+    private String email;
+    @Embedded
+    private Endereco endereco;
+    private Boolean ativo;
+
+    public Paciente(DadosCadastroPaciente dados) {
+        this.nome = dados.nome();
+        this.cpf = dados.cpf();
+        this.telefone = dados.telefone();
+        this.email = dados.email();
+        this.endereco = new Endereco(dados.endereco());
+        this.ativo = true;
+    }
 }
