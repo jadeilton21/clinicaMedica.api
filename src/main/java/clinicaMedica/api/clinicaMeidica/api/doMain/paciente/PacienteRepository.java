e5c32585f2a0bf69615d1096dfc61b6d1d5ea683
jadeilton <jadeiltondm@gmail.com>
2023-12-05 09:45:12 -0300

    feat: create interface PacienteRepository

---
+++ src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/paciente/PacienteRepository.java
@@ -0,0 +1,6 @@
+package clinicaMedica.api.clinicaMeidica.api.doMain.paciente;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PacienteRepository extends JpaRepository<Paciente,Long> {
+}
