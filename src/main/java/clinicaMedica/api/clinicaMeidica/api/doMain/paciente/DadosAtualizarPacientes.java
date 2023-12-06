e8b69d4ce5eba8c77ce61b929fb1faa50ae2d553
jadeilton <jadeiltondm@gmail.com>
2023-12-06 11:17:42 -0300

    feat: update record DadosAtualizarPacientes

--- src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/paciente/DadosAtualizarPacientes.java
+++ src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/paciente/DadosAtualizarPacientes.java
@@ -1,4 +1,13 @@
 package clinicaMedica.api.clinicaMeidica.api.doMain.paciente;
 
-public record DadosAtualizarPacientes() {
+import clinicaMedica.api.clinicaMeidica.api.doMain.endereco.DadosEndereco;
+import jakarta.validation.constraints.NotNull;
+
+public record DadosAtualizarPacientes(
+        @NotNull
+        Long id,
+        String nome,
+        String telefone,
+        DadosEndereco endereco
+) {
 }
