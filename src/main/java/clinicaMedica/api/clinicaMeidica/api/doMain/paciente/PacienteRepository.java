59cfd8e437eb57a327e2d78d8fe3bb2475f3c811
jadeilton <jadeiltondm@gmail.com>
2023-12-05 21:14:35 -0300

    feat: upadte Interface PacienteRepository

--- src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/paciente/PacienteRepository.java
+++ src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/paciente/PacienteRepository.java
@@ -1,10 +1,9 @@
 package clinicaMedica.api.clinicaMeidica.api.doMain.paciente;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.awt.print.Pageable;
-import java.util.Optional;
 
 
 public interface PacienteRepository extends JpaRepository<Paciente,Long> {
