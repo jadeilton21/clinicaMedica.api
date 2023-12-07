67a133ebb0e8a9e39c6ec5eac836414d740b3b30
jadeilton <jadeiltondm@gmail.com>
2023-12-07 10:39:41 -0300

    feat: create class FuncionarioController

--- src/main/java/clinicaMedica/api/clinicaMeidica/api/controller/FuncionarioController.java
+++ src/main/java/clinicaMedica/api/clinicaMeidica/api/controller/FuncionarioController.java
@@ -1,4 +1,15 @@
 package clinicaMedica.api.clinicaMeidica.api.controller;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("funcionarios")
+@SecurityRequirement(name = "bearer-key")
 public class FuncionarioController {
+
+
+
+
 }
