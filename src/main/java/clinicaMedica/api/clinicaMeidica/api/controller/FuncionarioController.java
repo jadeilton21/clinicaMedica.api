b41515381724c6070fc503f0e1b0a2a29a825552
jadeilton <jadeiltondm@gmail.com>
2023-12-07 14:35:04 -0300

    feat: create method cadastrarFuncionario in class FuncionarioController

--- src/main/java/clinicaMedica/api/clinicaMeidica/api/controller/FuncionarioController.java
+++ src/main/java/clinicaMedica/api/clinicaMeidica/api/controller/FuncionarioController.java
@@ -1,6 +1,11 @@
 package clinicaMedica.api.clinicaMeidica.api.controller;
 
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class FuncionarioController {
 
 
+    @PostMapping
+    @Transactional
+    public ResponseEntity cadastrarFuncionario(@RequestBody @Valid DadosCadastrarFuncionario dados){
+
+
+    }
 
 
 }
