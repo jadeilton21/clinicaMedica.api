eb49f633b8b763fef41f7caf540b45b8991747c4
jadeilton <jadeiltondm@gmail.com>
2023-12-08 13:41:33 -0300

    feat:create method findAllByAtivoTrue in interface FuncionarioRespository

--- src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/funcionario/FuncionarioRespository.java
+++ src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/funcionario/FuncionarioRespository.java
@@ -1,6 +1,11 @@
 package clinicaMedica.api.clinicaMeidica.api.doMain.funcionario;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface FuncionarioRespository extends JpaRepository<Funcionario,Long> {
+    Page<Funcionario> findAllByAtivoTrue(Pageable paginacao);
 }
