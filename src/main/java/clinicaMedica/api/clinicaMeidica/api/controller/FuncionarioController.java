6bfd8d12238a2ea08a9fe16a2496c840440b2c54
jadeilton <jadeiltondm@gmail.com>
2023-12-08 13:21:47 -0300

    feat:update class FuncionarioController

--- src/main/java/clinicaMedica/api/clinicaMeidica/api/controller/FuncionarioController.java
+++ src/main/java/clinicaMedica/api/clinicaMeidica/api/controller/FuncionarioController.java
@@ -1,11 +1,13 @@
 package clinicaMedica.api.clinicaMeidica.api.controller;
 
 import clinicaMedica.api.clinicaMeidica.api.doMain.funcionario.DadosCadastrarFuncionario;
+import clinicaMedica.api.clinicaMeidica.api.doMain.funcionario.DadosListagemFuncionario;
 import clinicaMedica.api.clinicaMeidica.api.doMain.funcionario.Funcionario;
 import clinicaMedica.api.clinicaMeidica.api.doMain.funcionario.FuncionarioRespository;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,6 +30,12 @@ public class FuncionarioController {
     public void cadastrarFuncionario(@RequestBody @Valid DadosCadastrarFuncionario dados){
 
         funcionarioRespository.save(new Funcionario(dados));
+
+    }
+
+
+
+    public Page<DadosListagemFuncionario> listarFuncionario(@RequestBody@Valid DadosListagemFuncionario dados){
 
     }
 
