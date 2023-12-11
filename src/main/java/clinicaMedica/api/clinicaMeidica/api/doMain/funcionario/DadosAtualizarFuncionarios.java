b119175812219b66e3a5ceeade0d08e710d41a82
jadeilton <jadeiltondm@gmail.com>
2023-12-11 07:50:49 -0300

    feat: update record DadosAtualizarFuncionarios

--- src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/funcionario/DadosAtualizarFuncionarios.java
+++ src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/funcionario/DadosAtualizarFuncionarios.java
@@ -1,4 +1,13 @@
 package clinicaMedica.api.clinicaMeidica.api.doMain.funcionario;
 
-public record DadosAtualizarFuncionarios() {
+import clinicaMedica.api.clinicaMeidica.api.doMain.endereco.DadosEndereco;
+import jakarta.validation.constraints.NotNull;
+
+public record DadosAtualizarFuncionarios(
+        @NotNull
+        Long id,
+        String nome,
+        String telefone,
+        DadosEndereco endereco
+) {
 }
