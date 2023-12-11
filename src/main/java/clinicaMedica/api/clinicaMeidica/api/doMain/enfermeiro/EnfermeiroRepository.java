7f3bf130390cb81a7685e4f30a1287890236a8a5
jadeilton <jadeiltondm@gmail.com>
2023-12-11 11:06:35 -0300

    feat: create method findAllByAtivoTrue in Interface EnfermeiroRepository

--- src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/enfermeiro/EnfermeiroRepository.java
+++ src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/enfermeiro/EnfermeiroRepository.java
@@ -1,6 +1,9 @@
 package clinicaMedica.api.clinicaMeidica.api.doMain.enfermeiro;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EnfermeiroRepository extends JpaRepository<Enfermeiro,Long> {
+    Page<Enfermeiro> findAllByAtivoTrue(Pageable pageable);
 }
