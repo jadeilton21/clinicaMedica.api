39787439b2c84c15bf82bdada2deb5d5ceeb85e0
jadeilton <jadeiltondm@gmail.com>
2023-12-11 13:52:04 -0300

    feat:create method atualizarInformações in class Enfermeiros

--- src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/enfermeiro/Enfermeiro.java
+++ src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/enfermeiro/Enfermeiro.java
@@ -40,4 +40,16 @@ public class Enfermeiro {
         this.endereco = endereco;
         this.especialidadeDoEnfermeiro = especialidadeDoEnfermeiro;
     }
+
+    public void atualizarInformacoes(DadosAtualizarEnfermeiros dados) {
+        if (dados.nome() != null) {
+            this.nome = dados.nome();
+        }
+        if (dados.telefone() != null) {
+            this.telefone = dados.telefone();
+        }
+        if (dados.endereco() != null) {
+            this.endereco.atualizarInformacoes(dados.endereco());
+        }
+    }
 }
