9b1048a6f4b997863a04f037c9c4bc4902082bfd
jadeilton <jadeiltondm@gmail.com>
2023-12-11 10:16:17 -0300

    feat: create interface EnfermeiroRepository

---
+++ src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/enfermeiro/EnfermeiroRepository.java
@@ -0,0 +1,6 @@
+package clinicaMedica.api.clinicaMeidica.api.doMain.enfermeiro;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EnfermeiroRepository extends JpaRepository<Enfermeiro,Long> {
+}
