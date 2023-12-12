8944338f8334d1788697aed85e7c2dc3e9b050de
jadeilton <jadeiltondm@gmail.com>
2023-12-12 16:11:44 -0300

    feat: create Enum MotivoDoCancelamento

---
+++ src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/consulta/MotivoDoCancelamento.java
@@ -0,0 +1,9 @@
+package clinicaMedica.api.clinicaMeidica.api.doMain.consulta;
+
+public enum MotivoDoCancelamento {
+
+    PACIENTE_DESISTIU,
+    ENFERMEIRO_CANCELOU,
+    MEDICO_CANCELOU,
+    OUTROS;
+}
