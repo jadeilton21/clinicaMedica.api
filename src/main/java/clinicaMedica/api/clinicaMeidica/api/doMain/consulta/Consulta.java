d2147560e0b57b834ea4dac6f347bbcfe1fa3803
jadeilton <jadeiltondm@gmail.com>
2023-12-12 16:10:01 -0300

    feat: update in class Consulta, creating Constructor in class Consulta, creating method cancelar in class Consulta.

--- src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/consulta/Consulta.java
+++ src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/consulta/Consulta.java
@@ -1,4 +1,59 @@
 package clinicaMedica.api.clinicaMeidica.api.doMain.consulta;
 
+
+import clinicaMedica.api.clinicaMeidica.api.doMain.enfermeiro.Enfermeiro;
+import clinicaMedica.api.clinicaMeidica.api.doMain.medico.Medico;
+import clinicaMedica.api.clinicaMeidica.api.doMain.paciente.Paciente;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.print.attribute.standard.Media;
+import java.time.LocalDateTime;
+
+@Table(name = "consultas")
+@Entity(name = "Consulta")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Consulta {
+
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "medico_id")
+    private Medico medico;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "enfermeiro_id")
+    private Enfermeiro enfermeiro;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "paciente_id")
+    private Paciente paciente;
+
+    private LocalDateTime localDateTime;
+    @Column(name = "motivo_cancelamento")
+    @Enumerated(EnumType.STRING)
+    private MotivoDoCancelamento motivoDoCancelamento;
+
+
+    public Consulta(Long id, Medico medico, Enfermeiro enfermeiro, Paciente paciente, LocalDateTime localDateTime) {
+        this.id = id;
+        this.medico = medico;
+        this.enfermeiro = enfermeiro;
+        this.paciente = paciente;
+        this.localDateTime = localDateTime;
+    }
+
+
+    public void cancelar(MotivoDoCancelamento motivo){
+        this.motivoDoCancelamento = motivo;
+    }
+
+
+
 }
