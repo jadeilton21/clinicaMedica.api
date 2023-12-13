75014d6dd4a6a922f4daf7c5272f8fc79be1a663
jadeilton <jadeiltondm@gmail.com>
2023-12-13 18:57:21 -0300

    feat: create ValidadorAgendamentoDeConsultas

---
+++ src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/consulta/validacoes/agendamento/ValidadorAgendamentoDeConsultas.java
@@ -0,0 +1,8 @@
+package clinicaMedica.api.clinicaMeidica.api.doMain.consulta.validacoes.agendamento;
+
+import clinicaMedica.api.clinicaMeidica.api.doMain.consulta.DadosAgendamentoConsulta;
+
+public interface ValidadorAgendamentoDeConsultas {
+
+    void validar(DadosAgendamentoConsulta dados);
+}
