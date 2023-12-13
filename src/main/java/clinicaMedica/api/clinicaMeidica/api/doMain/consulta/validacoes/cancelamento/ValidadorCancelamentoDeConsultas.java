2e754cbc61eaa1f840f6cd6c991564efe8fa9879
jadeilton <jadeiltondm@gmail.com>
2023-12-13 18:57:43 -0300

    feat: create ValidadorCancelamentoDeConsultas

---
+++ src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/consulta/validacoes/cancelamento/ValidadorCancelamentoDeConsultas.java
@@ -0,0 +1,7 @@
+package clinicaMedica.api.clinicaMeidica.api.doMain.consulta.validacoes.cancelamento;
+
+import clinicaMedica.api.clinicaMeidica.api.doMain.consulta.DadosCancelamentoConsulta;
+
+public interface ValidadorCancelamentoDeConsultas {
+    void validar(DadosCancelamentoConsulta dados);
+}
