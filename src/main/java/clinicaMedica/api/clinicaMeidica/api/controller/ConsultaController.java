03feabb3e079d0954d39449cbb489c9868666800
jadeilton <jadeiltondm@gmail.com>
2023-12-13 13:56:03 -0300

    feat: create ConsultaController

--- src/main/java/clinicaMedica/api/clinicaMeidica/api/controller/ConsultaController.java
+++ src/main/java/clinicaMedica/api/clinicaMeidica/api/controller/ConsultaController.java
@@ -1,4 +1,20 @@
 package clinicaMedica.api.clinicaMeidica.api.controller;
 
+import clinicaMedica.api.clinicaMeidica.api.doMain.consulta.AgendaDeConsultas;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("consultas")
+@SecurityRequirement(name = "bearer-key")
 public class ConsultaController {
+
+
+
+    @Autowired
+    private AgendaDeConsultas agendaDeConsultas;
+
+
 }
