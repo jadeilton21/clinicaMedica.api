08070fc6259f10eadc5dd3f3a32d5def0e5a031c
jadeilton <jadeiltondm@gmail.com>
2023-12-17 19:42:17 -0300

    feat: create class ValidadorHorarioFuncionamentoClinica

---
+++ src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/consulta/validacoes/agendamento/ValidadorHorarioFuncionamentoClinica.java
@@ -0,0 +1,12 @@
+package clinicaMedica.api.clinicaMeidica.api.doMain.consulta.validacoes.agendamento;
+
+import clinicaMedica.api.clinicaMeidica.api.doMain.consulta.DadosAgendamentoConsulta;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ValidadorHorarioFuncionamentoClinica implements ValidadorAgendamentoDeConsultas{
+    @Override
+    public void validar(DadosAgendamentoConsulta dados) {
+
+    }
+}
