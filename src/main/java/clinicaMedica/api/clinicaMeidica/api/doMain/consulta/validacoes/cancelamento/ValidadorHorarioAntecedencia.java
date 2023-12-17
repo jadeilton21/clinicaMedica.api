3e781509a6c1b0c35abc747db0adffc191b2b704
jadeilton <jadeiltondm@gmail.com>
2023-12-17 19:28:42 -0300

    feat: Upadate class ValidadorHorarioAntecedencia

--- src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/consulta/validacoes/cancelamento/ValidadorHorarioAntecedencia.java
+++ src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/consulta/validacoes/cancelamento/ValidadorHorarioAntecedencia.java
@@ -1,8 +1,18 @@
 package clinicaMedica.api.clinicaMeidica.api.doMain.consulta.validacoes.cancelamento;
 
 
+import clinicaMedica.api.clinicaMeidica.api.doMain.consulta.ConsultaRepository;
+import clinicaMedica.api.clinicaMeidica.api.doMain.consulta.DadosCancelamentoConsulta;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component("ValidadorHorarioAntecedenciaCancelamento")
 public class ValidadorHorarioAntecedencia implements ValidadorCancelamentoDeConsultas{
+    @Autowired
+    private ConsultaRepository consultaRepository;
+
+    @Override
+    public void validar(DadosCancelamentoConsulta dados) {
+
+    }
 }
