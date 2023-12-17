eefd91395a2b908042a02d66d8458e4fcea1403c
jadeilton <jadeiltondm@gmail.com>
2023-12-17 19:33:59 -0300

    feat: create method validar in class ValidadorHorarioAntecedencia

--- src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/consulta/validacoes/cancelamento/ValidadorHorarioAntecedencia.java
+++ src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/consulta/validacoes/cancelamento/ValidadorHorarioAntecedencia.java
@@ -1,10 +1,14 @@
 package clinicaMedica.api.clinicaMeidica.api.doMain.consulta.validacoes.cancelamento;
 
 
+import clinicaMedica.api.clinicaMeidica.api.doMain.ValidacaoException;
 import clinicaMedica.api.clinicaMeidica.api.doMain.consulta.ConsultaRepository;
 import clinicaMedica.api.clinicaMeidica.api.doMain.consulta.DadosCancelamentoConsulta;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
 
 @Component("ValidadorHorarioAntecedenciaCancelamento")
 public class ValidadorHorarioAntecedencia implements ValidadorCancelamentoDeConsultas{
@@ -13,6 +17,12 @@ public class ValidadorHorarioAntecedencia implements ValidadorCancelamentoDeCons
 
     @Override
     public void validar(DadosCancelamentoConsulta dados) {
+        var consulta = consultaRepository.getReferenceById(dados.idConsulta());
+        var agora= LocalDateTime.now();
+        var diferencaEmHoras    = Duration.between(agora,consulta.getData()).toHours();
 
+        if(diferencaEmHoras < 24){
+            throw new ValidacaoException("Consulta somente pode ser cancelada com antecendência minima de 24h!");
+        }
     }
 }
