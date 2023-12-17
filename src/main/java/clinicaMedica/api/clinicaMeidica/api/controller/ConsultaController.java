934bcd52254dc9749552548bc933ebf723a4325b
jadeilton <jadeiltondm@gmail.com>
2023-12-17 19:20:13 -0300

    feat: create methods cancelar is agendar in class ConsultaController

--- src/main/java/clinicaMedica/api/clinicaMeidica/api/controller/ConsultaController.java
+++ src/main/java/clinicaMedica/api/clinicaMeidica/api/controller/ConsultaController.java
@@ -2,15 +2,13 @@ package clinicaMedica.api.clinicaMeidica.api.controller;
 
 import clinicaMedica.api.clinicaMeidica.api.doMain.consulta.AgendaDeConsultas;
 import clinicaMedica.api.clinicaMeidica.api.doMain.consulta.DadosAgendamentoConsulta;
+import clinicaMedica.api.clinicaMeidica.api.doMain.consulta.DadosCancelamentoConsulta;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("consultas")
@@ -26,6 +24,13 @@ public class ConsultaController {
     public ResponseEntity agendar(@RequestBody @Valid DadosAgendamentoConsulta dados){
         var dto = agenda.agendar(dados);
         return ResponseEntity.ok(dto);
+    }
+    @DeleteMapping
+    @Transactional
+    public ResponseEntity cancelar(@RequestBody @Valid DadosCancelamentoConsulta dados){
+        agenda.cancelar(dados);
+        return ResponseEntity.noContent().build();
+
     }
 
 
