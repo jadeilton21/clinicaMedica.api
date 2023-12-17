ba71d8cdffcfb3f087859ee62f2760ead99ba9aa
jadeilton <jadeiltondm@gmail.com>
2023-12-17 20:02:53 -0300

    feat: create method validar in class ValidadorHorarioFuncionamentoClinica

--- src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/consulta/validacoes/agendamento/ValidadorHorarioFuncionamentoClinica.java
+++ src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/consulta/validacoes/agendamento/ValidadorHorarioFuncionamentoClinica.java
@@ -1,12 +1,24 @@
 package clinicaMedica.api.clinicaMeidica.api.doMain.consulta.validacoes.agendamento;
 
+import clinicaMedica.api.clinicaMeidica.api.doMain.ValidacaoException;
 import clinicaMedica.api.clinicaMeidica.api.doMain.consulta.DadosAgendamentoConsulta;
 import org.springframework.stereotype.Component;
+
+import java.time.DayOfWeek;
 
 @Component
 public class ValidadorHorarioFuncionamentoClinica implements ValidadorAgendamentoDeConsultas{
     @Override
     public void validar(DadosAgendamentoConsulta dados) {
+        var dataCOnsulta = dados.data();
+        var domingo = dataCOnsulta.getDayOfWeek().equals(DayOfWeek.FRIDAY);
+        var antesDaAberturaDaClinica = dataCOnsulta.getHour() < 7;
+        var depoisDoEncerramentoDaClinica = dataCOnsulta.getHour() > 18;
 
+        if(domingo || antesDaAberturaDaClinica || depoisDoEncerramentoDaClinica){
+            throw new ValidacaoException("Consulta fora do horário de funcionamento da clínica.");
+
+
+        }
     }
 }
