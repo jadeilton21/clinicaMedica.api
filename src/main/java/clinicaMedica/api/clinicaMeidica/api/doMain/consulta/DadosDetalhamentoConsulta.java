a25766b249a5af1ac79257496aeee8bd68a13d7d
jadeilton <jadeiltondm@gmail.com>
2023-12-17 16:21:14 -0300

    feat: update record DadosDetalhamentoConsulta

--- src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/consulta/DadosDetalhamentoConsulta.java
+++ src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/consulta/DadosDetalhamentoConsulta.java
@@ -3,7 +3,8 @@ package clinicaMedica.api.clinicaMeidica.api.doMain.consulta;
 
 import java.time.LocalDateTime;
 
-public record DadosDetalhamentoConsulta(Long id, Long idMedico,Long idPaciente, Long idEnfermeiro, LocalDateTime data) {
+public record DadosDetalhamentoConsulta(Long id, Long idMedico,
+                                        clinicaMedica.api.clinicaMeidica.api.doMain.enfermeiro.Enfermeiro idPaciente, Long idEnfermeiro, LocalDateTime data) {
     public DadosDetalhamentoConsulta(Consulta consulta) {
         this(consulta.getId(),consulta.getMedico().getId(),consulta.getEnfermeiro(),consulta.getPaciente().getId(),consulta.getData());
     }
