369f5d4d5b6bcdd434de7b2ef6395b5012027e9c
jadeilton <jadeiltondm@gmail.com>
2023-12-18 15:43:54 -0300

    feat: create method validar in class ValidadorMedicoComOutraConsultaNoMesmoHorario

--- src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/consulta/validacoes/agendamento/ValidadorMedicoComOutraConsultaNoMesmoHorario.java
+++ src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/consulta/validacoes/agendamento/ValidadorMedicoComOutraConsultaNoMesmoHorario.java
@@ -1,8 +1,18 @@
 package clinicaMedica.api.clinicaMeidica.api.doMain.consulta.validacoes.agendamento;
 
 
+import clinicaMedica.api.clinicaMeidica.api.doMain.consulta.ConsultaRepository;
+import clinicaMedica.api.clinicaMeidica.api.doMain.consulta.DadosAgendamentoConsulta;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ValidadorMedicoComOutraConsultaNoMesmoHorario implements ValidadorAgendamentoDeConsultas{
+    @Autowired
+    private ConsultaRepository consultaRepository;
+
+    @Override
+    public void validar(DadosAgendamentoConsulta dados) {
+
+    }
 }
