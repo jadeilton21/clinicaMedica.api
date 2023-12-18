71651a410418da785afc44361e2941ba74c9e20d
jadeilton <jadeiltondm@gmail.com>
2023-12-18 16:47:22 -0300

    feat: update in class ValidadorMedicoComOutraConsultaNoMesmoHorario

--- src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/consulta/validacoes/agendamento/ValidadorMedicoComOutraConsultaNoMesmoHorario.java
+++ src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/consulta/validacoes/agendamento/ValidadorMedicoComOutraConsultaNoMesmoHorario.java
@@ -1,18 +1,25 @@
 package clinicaMedica.api.clinicaMeidica.api.doMain.consulta.validacoes.agendamento;
 
 
+import clinicaMedica.api.clinicaMeidica.api.doMain.ValidacaoException;
 import clinicaMedica.api.clinicaMeidica.api.doMain.consulta.ConsultaRepository;
 import clinicaMedica.api.clinicaMeidica.api.doMain.consulta.DadosAgendamentoConsulta;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class ValidadorMedicoComOutraConsultaNoMesmoHorario implements ValidadorAgendamentoDeConsultas{
+public class ValidadorMedicoComOutraConsultaNoMesmoHorario implements ValidadorAgendamentoDeConsultas {
     @Autowired
     private ConsultaRepository consultaRepository;
 
     @Override
     public void validar(DadosAgendamentoConsulta dados) {
+
+        var medicoPossuiUmaOutraConsultaNesseHorario = consultaRepository.existsByMedicoIdAndDataAndMotivoCancelamentoIsNull(dados.idMedico(), dados.data());
+        if (!medicoPossuiUmaOutraConsultaNesseHorario) {
+            throw new ValidacaoException("Médico já possui uma Consulta agendada nesse Momento.");
+        }
+
 
     }
 }
