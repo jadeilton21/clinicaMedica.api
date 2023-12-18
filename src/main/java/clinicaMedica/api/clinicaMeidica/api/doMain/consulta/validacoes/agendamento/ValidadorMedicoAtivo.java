8e6823089af9b57bcd67bfe8b6abba7d163563c2
jadeilton <jadeiltondm@gmail.com>
2023-12-18 15:37:51 -0300

    feat: create argument in method validar in class ValidadorMedicoAtivo

--- src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/consulta/validacoes/agendamento/ValidadorMedicoAtivo.java
+++ src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/consulta/validacoes/agendamento/ValidadorMedicoAtivo.java
@@ -1,6 +1,7 @@
 package clinicaMedica.api.clinicaMeidica.api.doMain.consulta.validacoes.agendamento;
 
 
+import clinicaMedica.api.clinicaMeidica.api.doMain.ValidacaoException;
 import clinicaMedica.api.clinicaMeidica.api.doMain.consulta.DadosAgendamentoConsulta;
 import clinicaMedica.api.clinicaMeidica.api.doMain.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,17 @@ public class ValidadorMedicoAtivo {
 
 
     public void validar(DadosAgendamentoConsulta dados){
+
+        if(dados.idMedico() == null){
+            return;
+        }
+
+        var medicoEstaAtivo = medicoRepository.findAllById(dados.idMedico());
+        if(!medicoEstaAtivo){
+            throw new ValidacaoException("Consulta não pode ser agendada com o Médico.");
+        }
+
+
 
     }
 }
