b2414b9679f0c22addae653f9e9a7138a489d48c
jadeilton <jadeiltondm@gmail.com>
2023-12-18 15:41:13 -0300

    feat: create class ValidadorMedicoComOutraConsultaNoMesmoHorario

---
+++ src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/consulta/validacoes/agendamento/ValidadorMedicoComOutraConsultaNoMesmoHorario.java
@@ -0,0 +1,8 @@
+package clinicaMedica.api.clinicaMeidica.api.doMain.consulta.validacoes.agendamento;
+
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ValidadorMedicoComOutraConsultaNoMesmoHorario implements ValidadorAgendamentoDeConsultas{
+}
