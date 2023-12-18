c3a9aba6e65e150bc733ad614723e3b23294fcfd
jadeilton <jadeiltondm@gmail.com>
2023-12-18 15:34:33 -0300

    feat: create method validar in class ValidadorMedicoAtivo

--- src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/consulta/validacoes/agendamento/ValidadorMedicoAtivo.java
+++ src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/consulta/validacoes/agendamento/ValidadorMedicoAtivo.java
@@ -1,4 +1,20 @@
 package clinicaMedica.api.clinicaMeidica.api.doMain.consulta.validacoes.agendamento;
 
+
+import clinicaMedica.api.clinicaMeidica.api.doMain.consulta.DadosAgendamentoConsulta;
+import clinicaMedica.api.clinicaMeidica.api.doMain.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class ValidadorMedicoAtivo {
+
+
+    @Autowired
+    private MedicoRepository medicoRepository;
+
+
+    public void validar(DadosAgendamentoConsulta dados){
+
+    }
 }
