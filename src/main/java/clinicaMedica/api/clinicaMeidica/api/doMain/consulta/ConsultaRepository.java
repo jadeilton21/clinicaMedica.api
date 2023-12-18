fb36023927b96014b2b380128132b05a1c72fa57
jadeilton <jadeiltondm@gmail.com>
2023-12-18 16:49:09 -0300

    feat: create method existsByMedicoIdAndDataAndMotivoCancelamentoIsNull

--- src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/consulta/ConsultaRepository.java
+++ src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/consulta/ConsultaRepository.java
@@ -2,5 +2,8 @@ package clinicaMedica.api.clinicaMeidica.api.doMain.consulta;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+
 public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
+    Boolean existsByMedicoIdAndDataAndMotivoCancelamentoIsNull(Long idMedico, LocalDateTime data);
 }
