8405ba3bc06a05d7960b1d44d78a364fa3491b06
jadeilton <jadeiltondm@gmail.com>
2023-12-18 15:17:33 -0300

    feat: create arguments in method validar.

--- src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/consulta/validacoes/agendamento/ValidadorHorarioAntecendencia.java
+++ src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/consulta/validacoes/agendamento/ValidadorHorarioAntecendencia.java
@@ -1,8 +1,12 @@
 package clinicaMedica.api.clinicaMeidica.api.doMain.consulta.validacoes.agendamento;
 
 
+import clinicaMedica.api.clinicaMeidica.api.doMain.ValidacaoException;
 import clinicaMedica.api.clinicaMeidica.api.doMain.consulta.DadosAgendamentoConsulta;
 import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
 
 @Component("ValidadorHorarioAntecendenciaAgendamento")
 public class ValidadorHorarioAntecendencia implements ValidadorAgendamentoDeConsultas{
@@ -10,6 +14,16 @@ public class ValidadorHorarioAntecendencia implements ValidadorAgendamentoDeCons
 
     @Override
     public void validar(DadosAgendamentoConsulta dados) {
+
+
+        var dataConsulta = dados.data();
+        var agora = LocalDateTime.now();
+        var diferencaEmMinutos = Duration.between(agora,dataConsulta).toMinutes();
+
+        if(diferencaEmMinutos < 30){
+            throw new ValidacaoException("Consulta deve ser agendada com antecedência mínima de 30 minuto");
+
+        }
 
     }
 }
