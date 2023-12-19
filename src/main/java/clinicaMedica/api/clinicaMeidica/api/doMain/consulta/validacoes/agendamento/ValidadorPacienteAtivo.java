829e7893db8254a403ce0d2f1f153cd73e5578f0
jadeilton <jadeiltondm@gmail.com>
2023-12-19 08:35:41 -0300

    feat: Update class ValidadorPacienteAtivo

--- src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/consulta/validacoes/agendamento/ValidadorPacienteAtivo.java
+++ src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/consulta/validacoes/agendamento/ValidadorPacienteAtivo.java
@@ -1,5 +1,6 @@
 package clinicaMedica.api.clinicaMeidica.api.doMain.consulta.validacoes.agendamento;
 
+import clinicaMedica.api.clinicaMeidica.api.doMain.ValidacaoException;
 import clinicaMedica.api.clinicaMeidica.api.doMain.consulta.DadosAgendamentoConsulta;
 import clinicaMedica.api.clinicaMeidica.api.doMain.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +14,13 @@ public class ValidadorPacienteAtivo {
 
 
     public void validar(DadosAgendamentoConsulta dados){
-
+        if(dados.idPaciente() == null){
+            return;
+        }
+        var pacienteEstaAtivo = pacienteRepository.findAllById(dados.idPaciente());
+        if(!pacienteEstaAtivo){
+            throw new ValidacaoException("Consulta não pode ser Agendada com o Paciente Excluido.");
+        }
     }
 
 
