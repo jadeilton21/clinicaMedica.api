cf8919cf31979d224964c3c67c6a4ed82a78418a
jadeilton <jadeiltondm@gmail.com>
2023-12-19 08:54:35 -0300

    feat: create arguments in method validar

--- src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/consulta/validacoes/agendamento/ValidadorPacienteSemOutraConsultaNoDia.java
+++ src/main/java/clinicaMedica/api/clinicaMeidica/api/doMain/consulta/validacoes/agendamento/ValidadorPacienteSemOutraConsultaNoDia.java
@@ -1,6 +1,7 @@
 package clinicaMedica.api.clinicaMeidica.api.doMain.consulta.validacoes.agendamento;
 
 
+import clinicaMedica.api.clinicaMeidica.api.doMain.ValidacaoException;
 import clinicaMedica.api.clinicaMeidica.api.doMain.consulta.ConsultaRepository;
 import clinicaMedica.api.clinicaMeidica.api.doMain.consulta.DadosAgendamentoConsulta;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,14 @@ public class ValidadorPacienteSemOutraConsultaNoDia implements ValidadorAgendame
 
     @Override
     public void validar(DadosAgendamentoConsulta dados) {
+        var primeiroHorario = dados.data().withHour(7);
+        var ultimoHorario = dados.data().withHour(18);
+
+        var pacientePossuiConsultaNoDia = consultaRepository.existsByPacienteIdAndDataBetween(dados.idPaciente(),primeiroHorario,ultimoHorario);
+
+        if(!pacientePossuiConsultaNoDia){
+            throw new ValidacaoException("Paciente já possui agendamento marcado para esse Hórario.");
+        }
 
     }
 }
