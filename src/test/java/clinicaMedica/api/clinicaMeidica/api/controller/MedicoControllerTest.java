66e4970fb353ed02becac18aa8cd686ed0742d6f
jadeilton <jadeiltondm@gmail.com>
2023-12-20 09:58:39 -0300

    feat: create class test MedicoControllerTest, method cadastrarMedico

---
+++ src/test/java/clinicaMedica/api/clinicaMeidica/api/controller/MedicoControllerTest.java
@@ -0,0 +1,25 @@
+package clinicaMedica.api.clinicaMeidica.api.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureJsonTesters
+class MedicoControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Test
+    void cadastrarMedico() {
+    }
+}
