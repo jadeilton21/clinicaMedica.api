46c54d4afbf091f6431e79d685b135c141d420fe
jadeilton <jadeiltondm@gmail.com>
2023-12-20 10:32:44 -0300

    feat: method cadastrarMedico tested.

--- src/test/java/clinicaMedica/api/clinicaMeidica/api/controller/MedicoControllerTest.java
+++ src/test/java/clinicaMedica/api/clinicaMeidica/api/controller/MedicoControllerTest.java
@@ -1,15 +1,18 @@
 package clinicaMedica.api.clinicaMeidica.api.controller;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
-
-
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 
 @SpringBootTest
@@ -20,6 +23,20 @@ class MedicoControllerTest {
     @Autowired
     private MockMvc mockMvc;
     @Test
-    void cadastrarMedico() {
+    @WithMockUser
+    @DisplayName("Deve Devolver Código ")
+    void cadastrarMedico() throws Exception{
+
+        var response = mockMvc
+                .perform(post("/medicos"))
+                .andReturn().getResponse();
+
+
+        assertThat(response.getStatus())
+                .isEqualTo(HttpStatus.BAD_REQUEST.value());
+
+
+
+
     }
 }
