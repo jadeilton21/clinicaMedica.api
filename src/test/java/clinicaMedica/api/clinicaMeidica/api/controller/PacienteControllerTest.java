65d3b4d2b91f40419a73a81cb504f364ca69bcff
jadeilton <jadeiltondm@gmail.com>
2023-12-21 22:01:18 -0300

    feat: create arguments of method cadastrarPaciente_cenario1 in class PacienteControllerTest

--- src/test/java/clinicaMedica/api/clinicaMeidica/api/controller/PacienteControllerTest.java
+++ src/test/java/clinicaMedica/api/clinicaMeidica/api/controller/PacienteControllerTest.java
@@ -2,12 +2,17 @@ package clinicaMedica.api.clinicaMeidica.api.controller;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 
 @SpringBootTest
@@ -16,8 +21,21 @@ import static org.junit.jupiter.api.Assertions.*;
 @WithMockUser
 class PacienteControllerTest {
 
+
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
-    @DisplayName("")
-    void cadastrarPaciente_cenario1() {
+    @DisplayName("Deve Devolver Código 404 quando informações estiverem invalidas..")
+    void cadastrarPaciente_cenario1() throws Exception{
+
+
+        var response = mockMvc
+                .perform(post("/pacientes"))
+                .andReturn().getResponse();
+
+
+        assertThat(response.getStatus())
+                .isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 }
