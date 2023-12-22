f6fef1c2cdc102602d4c123c8efaaae262268d17
jadeilton <jadeiltondm@gmail.com>
2023-12-21 21:44:03 -0300

    feat: create class PacienteControllerTest

---
+++ src/test/java/clinicaMedica/api/clinicaMeidica/api/controller/PacienteControllerTest.java
@@ -0,0 +1,21 @@
+package clinicaMedica.api.clinicaMeidica.api.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureJsonTesters
+@WithMockUser
+class PacienteControllerTest {
+
+    @Test
+    void cadastrarPaciente() {
+    }
+}
