b41f45c6c358d0d93cf52f665923589825dc675d
jadeilton <jadeiltondm@gmail.com>
2023-12-23 14:15:07 -0300

    feat:create test metho cadastrarEnfermeiro.

---
+++ src/test/java/clinicaMedica/api/clinicaMeidica/api/controller/EnfermeiroControllerTest.java
@@ -0,0 +1,19 @@
+package clinicaMedica.api.clinicaMeidica.api.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@WithMockUser
+@AutoConfigureMockMvc
+
+class EnfermeiroControllerTest {
+
+    @Test
+    void cadastrarEnfermeiro() {
+    }
+}
