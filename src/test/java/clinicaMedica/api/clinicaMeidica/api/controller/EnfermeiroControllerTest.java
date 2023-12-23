3b2191531acecfe42d44ddc4407cbdfb97feb215
jadeilton <jadeiltondm@gmail.com>
2023-12-23 14:18:18 -0300

    feat:arguments method cadastrarEnfermeiro_cenario1.

--- src/test/java/clinicaMedica/api/clinicaMeidica/api/controller/EnfermeiroControllerTest.java
+++ src/test/java/clinicaMedica/api/clinicaMeidica/api/controller/EnfermeiroControllerTest.java
@@ -2,12 +2,15 @@ package clinicaMedica.api.clinicaMeidica.api.controller;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @SpringBootTest
 @WithMockUser
@@ -15,8 +18,14 @@ import static org.junit.jupiter.api.Assertions.*;
 @AutoConfigureJsonTesters
 class EnfermeiroControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
     @Test
     @DisplayName("Deve Devolver Código Htpp 404 quando informações forem invalidas")
-    void cadastrarEnfermeiro() {
+    void cadastrarEnfermeiro_cenario1() throws Exception {
+
+
+        var response = mockMvc.perform(post("/enfermeiros"))
+                .andReturn().getResponse();
     }
 }
