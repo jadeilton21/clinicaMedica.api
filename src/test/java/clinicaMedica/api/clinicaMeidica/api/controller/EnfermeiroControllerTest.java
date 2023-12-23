f7619e6ef05365306b8f6b648af9e050b63ebf9d
jadeilton <jadeiltondm@gmail.com>
2023-12-23 14:16:38 -0300

    feat:method cadastrarEnfermeiro_cenario1

--- src/test/java/clinicaMedica/api/clinicaMeidica/api/controller/EnfermeiroControllerTest.java
+++ src/test/java/clinicaMedica/api/clinicaMeidica/api/controller/EnfermeiroControllerTest.java
@@ -1,6 +1,8 @@
 package clinicaMedica.api.clinicaMeidica.api.controller;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
@@ -10,10 +12,11 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 @WithMockUser
 @AutoConfigureMockMvc
-
+@AutoConfigureJsonTesters
 class EnfermeiroControllerTest {
 
     @Test
+    @DisplayName("Deve Devolver Código Htpp 404 quando informações forem invalidas")
     void cadastrarEnfermeiro() {
     }
 }
