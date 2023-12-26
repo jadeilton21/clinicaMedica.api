b8517545a6d9fe140708024b0e1a6a0f09566888
jadeilton <jadeiltondm@gmail.com>
2023-12-26 20:19:21 -0300

    feat: create atributo MockMvc

--- src/test/java/clinicaMedica/api/clinicaMeidica/api/controller/FuncionarioControllerTest.java
+++ src/test/java/clinicaMedica/api/clinicaMeidica/api/controller/FuncionarioControllerTest.java
@@ -1,16 +1,22 @@
 package clinicaMedica.api.clinicaMeidica.api.controller;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.jdbc.AutoConfigureDataJdbc;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 @AutoConfigureMockMvc
 @WithMockUser
 class FuncionarioControllerTest {
+
+
+    @Autowired
+    private MockMvc mockMvc;
 
     @Test
     void cadastrarFuncionario() {
