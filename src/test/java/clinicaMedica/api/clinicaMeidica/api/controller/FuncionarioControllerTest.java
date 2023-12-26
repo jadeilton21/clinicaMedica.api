d43ad5416879b2c32052f4d054f0962579472432
jadeilton <jadeiltondm@gmail.com>
2023-12-26 20:14:23 -0300

    feat: CREATE CLASS TEST FuncionarioControllerTest

---
+++ src/test/java/clinicaMedica/api/clinicaMeidica/api/controller/FuncionarioControllerTest.java
@@ -0,0 +1,14 @@
+package clinicaMedica.api.clinicaMeidica.api.controller;
+
+import org.springframework.boot.test.autoconfigure.data.jdbc.AutoConfigureDataJdbc;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+@AutoConfigureMockMvc
+@WithMockUser
+class FuncionarioControllerTest {
+
+}
