b4f380471d798c191ffd876495958c39693e1de2
jadeilton <jadeiltondm@gmail.com>
2023-12-27 15:58:08 -0300

    feat: atributo MockMvc add

--- src/test/java/clinicaMedica/api/clinicaMeidica/api/controller/ConsultaControllerTest.java
+++ src/test/java/clinicaMedica/api/clinicaMeidica/api/controller/ConsultaControllerTest.java
@@ -2,13 +2,20 @@ package clinicaMedica.api.clinicaMeidica.api.controller;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ConsultaControllerTest {
 
+    @Autowired
+    private MockMvc mocMvc;
+
     @Test
     @DisplayName("Deve Devolver Código Http 404 quando informações forem invalidas...")
-    void agendar_cenario1() {
+    void agendar_cenario1() throws Exception {
+
+
     }
 }
