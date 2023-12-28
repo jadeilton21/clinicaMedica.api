d9a50d1577b5ad4fd1bf2646106c9e2bdf32168d
jadeilton <jadeiltondm@gmail.com>
2023-12-28 12:12:24 -0300

    feat: create class AutenticacaoControllerTest

---
+++ src/test/java/clinicaMedica/api/clinicaMeidica/api/controller/AutenticacaoControllerTest.java
@@ -0,0 +1,12 @@
+package clinicaMedica.api.clinicaMeidica.api.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AutenticacaoControllerTest {
+
+    @Test
+    void efetuarLogin() {
+    }
+}
