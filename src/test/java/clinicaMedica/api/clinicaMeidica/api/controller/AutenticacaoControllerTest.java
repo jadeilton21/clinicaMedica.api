dd2308b45cd07a7b333a21e90601dac9e5e40ebd
jadeilton <jadeiltondm@gmail.com>
2023-12-28 16:46:53 -0300

    feat: create atributo MockMvc.

--- src/test/java/clinicaMedica/api/clinicaMeidica/api/controller/AutenticacaoControllerTest.java
+++ src/test/java/clinicaMedica/api/clinicaMeidica/api/controller/AutenticacaoControllerTest.java
@@ -3,13 +3,26 @@ package clinicaMedica.api.clinicaMeidica.api.controller;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.DisabledOnJre;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@SpringBootTest
+@AutoConfigureMockMvc
+@WithMockUser
 class AutenticacaoControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
 
     @Test
     @DisplayName("Deve Devolver Código http 404 quando informações estiverem invalidas...")
     void efetuarLogin_cenario1() throws Exception {
+
+        var response = mockMvc
+                .perform(post("/login"))
     }
 }
