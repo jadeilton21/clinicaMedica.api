fa550f291a7ff95d078e4b4d4d0de9bd8dda1a85
jadeilton <jadeiltondm@gmail.com>
2023-12-28 12:42:52 -0300

    feat: create mwthod efetuaLogin_cenario1

--- src/test/java/clinicaMedica/api/clinicaMeidica/api/controller/AutenticacaoControllerTest.java
+++ src/test/java/clinicaMedica/api/clinicaMeidica/api/controller/AutenticacaoControllerTest.java
@@ -1,12 +1,15 @@
 package clinicaMedica.api.clinicaMeidica.api.controller;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnJre;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AutenticacaoControllerTest {
 
     @Test
-    void efetuarLogin() {
+    @DisplayName("Deve Devolver Código http 404 quando informações estiverem invalidas...")
+    void efetuarLogin_cenario1() throws Exception {
     }
 }
