b2ae39cb755ce95ca63f4d163a678cffcdd311d8
jadeilton <jadeiltondm@gmail.com>
2023-12-28 16:51:15 -0300

    feat: create arguments in method efetuarLogin_cenario1.

--- src/test/java/clinicaMedica/api/clinicaMeidica/api/controller/AutenticacaoControllerTest.java
+++ src/test/java/clinicaMedica/api/clinicaMeidica/api/controller/AutenticacaoControllerTest.java
@@ -6,10 +6,14 @@ import org.junit.jupiter.api.condition.DisabledOnJre;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
 @SpringBootTest
 @AutoConfigureMockMvc
 @WithMockUser
@@ -24,5 +28,10 @@ class AutenticacaoControllerTest {
 
         var response = mockMvc
                 .perform(post("/login"))
+                .andReturn().getResponse();
+
+
+        assertThat(response.getStatus())
+                .isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 }
