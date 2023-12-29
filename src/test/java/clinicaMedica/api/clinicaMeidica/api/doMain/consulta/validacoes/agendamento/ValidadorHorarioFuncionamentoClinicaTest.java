8915c09a305d9c7894a1e9ffa52971751dddf011
jadeilton <jadeiltondm@gmail.com>
2023-12-29 15:38:57 -0300

    feat: create test method validar in class ValidadorHorarioFuncionamentoClinicaTest

--- src/test/java/clinicaMedica/api/clinicaMeidica/api/doMain/consulta/validacoes/agendamento/ValidadorHorarioFuncionamentoClinicaTest.java
+++ src/test/java/clinicaMedica/api/clinicaMeidica/api/doMain/consulta/validacoes/agendamento/ValidadorHorarioFuncionamentoClinicaTest.java
@@ -1,5 +1,6 @@
 package clinicaMedica.api.clinicaMeidica.api.doMain.consulta.validacoes.agendamento;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,7 +17,7 @@ class ValidadorHorarioFuncionamentoClinicaTest {
     private MockMvc mockMvc;
 
 
-
-
-
+    @Test
+    void validar() {
+    }
 }
