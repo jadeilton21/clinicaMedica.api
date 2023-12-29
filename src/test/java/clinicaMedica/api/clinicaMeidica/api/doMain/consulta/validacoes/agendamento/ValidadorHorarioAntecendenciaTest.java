9aed5d057925367249b8999445d4773bc0e6a6ba
jadeilton <jadeiltondm@gmail.com>
2023-12-29 15:21:30 -0300

    feat: desenvolvend..

--- src/test/java/clinicaMedica/api/clinicaMeidica/api/doMain/consulta/validacoes/agendamento/ValidadorHorarioAntecendenciaTest.java
+++ src/test/java/clinicaMedica/api/clinicaMeidica/api/doMain/consulta/validacoes/agendamento/ValidadorHorarioAntecendenciaTest.java
@@ -1,7 +1,20 @@
 package clinicaMedica.api.clinicaMeidica.api.doMain.consulta.validacoes.agendamento;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+@AutoConfigureMockMvc
+@WithMockUser
 class ValidadorHorarioAntecendenciaTest {
+
+
+    @Autowired
+    private MockMvc mockMvc;
+
 
 }
