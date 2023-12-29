9e06fe53fe317f7531511e7a867204d8cfa39839
jadeilton <jadeiltondm@gmail.com>
2023-12-29 15:41:14 -0300

    feat: create arguments in method validar_cenario1.

--- src/test/java/clinicaMedica/api/clinicaMeidica/api/doMain/consulta/validacoes/agendamento/ValidadorHorarioFuncionamentoClinicaTest.java
+++ src/test/java/clinicaMedica/api/clinicaMeidica/api/doMain/consulta/validacoes/agendamento/ValidadorHorarioFuncionamentoClinicaTest.java
@@ -1,13 +1,18 @@
 package clinicaMedica.api.clinicaMeidica.api.doMain.consulta.validacoes.agendamento;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
 @SpringBootTest
 @AutoConfigureMockMvc
 @WithMockUser
@@ -18,6 +23,15 @@ class ValidadorHorarioFuncionamentoClinicaTest {
 
 
     @Test
-    void validar() {
+    @DisplayName("Deve Devolver Código Http 404 quando informações forem invalidas..")
+    void validar_cenario1() throws Exception {
+
+
+        var response = mockMvc.perform(
+                post("/consulta")
+        ).andReturn().getResponse();
+
+        assertThat(response.getStatus())
+                .isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 }
