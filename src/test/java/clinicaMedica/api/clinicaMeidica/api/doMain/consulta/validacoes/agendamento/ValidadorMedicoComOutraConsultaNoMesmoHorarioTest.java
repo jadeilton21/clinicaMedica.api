822efa93007a7094a0a2d2a265d8b517f74c98d7
jadeilton <jadeiltondm@gmail.com>
2023-12-30 17:49:26 -0300

    feat: create test ValidadorMedicoComOutraConsultaNoMesmoHorarioTest

--- src/test/java/clinicaMedica/api/clinicaMeidica/api/doMain/consulta/validacoes/agendamento/ValidadorMedicoComOutraConsultaNoMesmoHorarioTest.java
+++ src/test/java/clinicaMedica/api/clinicaMeidica/api/doMain/consulta/validacoes/agendamento/ValidadorMedicoComOutraConsultaNoMesmoHorarioTest.java
@@ -1,7 +1,20 @@
 package clinicaMedica.api.clinicaMeidica.api.doMain.consulta.validacoes.agendamento;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+
+@SpringBootTest
+@WithMockUser
+@AutoConfigureMockMvc
 class ValidadorMedicoComOutraConsultaNoMesmoHorarioTest {
+
+    @Autowired
+    private MockMvc mockMvc;
 
 }
