fff3ef717a402f65a35a77552d24dde136da7180
jadeilton <jadeiltondm@gmail.com>
2023-12-30 17:38:29 -0300

    feat: create test method validar_cenario1.

--- src/test/java/clinicaMedica/api/clinicaMeidica/api/doMain/consulta/validacoes/agendamento/ValidadorMedicoAtivoTest.java
+++ src/test/java/clinicaMedica/api/clinicaMeidica/api/doMain/consulta/validacoes/agendamento/ValidadorMedicoAtivoTest.java
@@ -1,5 +1,7 @@
 package clinicaMedica.api.clinicaMeidica.api.doMain.consulta.validacoes.agendamento;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,4 +17,9 @@ class ValidadorMedicoAtivoTest {
     @Autowired
     private MockMvc mockMvc;
 
+
+    @Test
+    @DisplayName("Deve Devolver Código Http 404 quando informações estiverem invalidas...")
+    void validar_cenario1() throws Exception {
+    }
 }
