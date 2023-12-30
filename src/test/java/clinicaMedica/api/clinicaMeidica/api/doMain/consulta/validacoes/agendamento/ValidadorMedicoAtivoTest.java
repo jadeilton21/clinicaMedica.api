87e0579facd1a43ebbdd8a841d2c70f0b50069d2
jadeilton <jadeiltondm@gmail.com>
2023-12-30 17:35:58 -0300

    feat: create class test ValidadorMedicoAtivoTest

--- src/test/java/clinicaMedica/api/clinicaMeidica/api/doMain/consulta/validacoes/agendamento/ValidadorMedicoAtivoTest.java
+++ src/test/java/clinicaMedica/api/clinicaMeidica/api/doMain/consulta/validacoes/agendamento/ValidadorMedicoAtivoTest.java
@@ -1,7 +1,18 @@
 package clinicaMedica.api.clinicaMeidica.api.doMain.consulta.validacoes.agendamento;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@WithMockUser
+@AutoConfigureMockMvc
 class ValidadorMedicoAtivoTest {
+    @Autowired
+    private MockMvc mockMvc;
 
 }
