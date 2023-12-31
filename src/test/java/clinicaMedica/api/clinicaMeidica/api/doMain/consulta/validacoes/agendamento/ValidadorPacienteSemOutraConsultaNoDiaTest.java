b6814def4393e5f0c490d41b2717f7b50fdb0ce2
jadeilton <jadeiltondm@gmail.com>
2023-12-31 14:55:51 -0300

    feat: method validar_cenario1 in class ValidadorPacienteSemOutraConsultaNoDiaTest

--- src/test/java/clinicaMedica/api/clinicaMeidica/api/doMain/consulta/validacoes/agendamento/ValidadorPacienteSemOutraConsultaNoDiaTest.java
+++ src/test/java/clinicaMedica/api/clinicaMeidica/api/doMain/consulta/validacoes/agendamento/ValidadorPacienteSemOutraConsultaNoDiaTest.java
@@ -1,5 +1,7 @@
 package clinicaMedica.api.clinicaMeidica.api.doMain.consulta.validacoes.agendamento;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,4 +20,8 @@ class ValidadorPacienteSemOutraConsultaNoDiaTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Test
+    @DisplayName("Deve Devolver Código Http 404 quando Informações forem Invalidas....")
+    void validar_cenario1()  throws Exception{
+    }
 }
