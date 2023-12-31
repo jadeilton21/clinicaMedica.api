66e1606cb845693a45d9efe1c4fc06ffb2538599
jadeilton <jadeiltondm@gmail.com>
2023-12-31 14:52:22 -0300

    feat: create test method ValidadorPacienteSemOutraConsultaNoDiaTest

---
+++ src/test/java/clinicaMedica/api/clinicaMeidica/api/doMain/consulta/validacoes/agendamento/ValidadorPacienteSemOutraConsultaNoDiaTest.java
@@ -0,0 +1,9 @@
+package clinicaMedica.api.clinicaMeidica.api.doMain.consulta.validacoes.agendamento;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+
+class ValidadorPacienteSemOutraConsultaNoDiaTest {
+
+}
