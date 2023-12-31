8f18499c0fda497c0906f5c27068cc725b32e7e6
jadeilton <jadeiltondm@gmail.com>
2023-12-31 14:53:03 -0300

    feat: create test method ValidadorPacienteSemOutraConsultaNoDiaTest

--- src/test/java/clinicaMedica/api/clinicaMeidica/api/doMain/consulta/validacoes/agendamento/ValidadorPacienteSemOutraConsultaNoDiaTest.java
+++ src/test/java/clinicaMedica/api/clinicaMeidica/api/doMain/consulta/validacoes/agendamento/ValidadorPacienteSemOutraConsultaNoDiaTest.java
@@ -1,9 +1,21 @@
 package clinicaMedica.api.clinicaMeidica.api.doMain.consulta.validacoes.agendamento;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
-
+@SpringBootTest
+@WithMockUser
+@AutoConfigureMockMvc
 class ValidadorPacienteSemOutraConsultaNoDiaTest {
+
+
+    @Autowired
+    private MockMvc mockMvc;
 
 }
