988373ffccd7a6721d0b9c9dcf1704c75e2eb47d
jadeilton <jadeiltondm@gmail.com>
2024-01-01 11:28:36 -0300

    feat: create arguments in method agendar_cenario1 in class AgendaDeConsultasTest

--- src/test/java/clinicaMedica/api/clinicaMeidica/api/doMain/consulta/AgendaDeConsultasTest.java
+++ src/test/java/clinicaMedica/api/clinicaMeidica/api/doMain/consulta/AgendaDeConsultasTest.java
@@ -5,10 +5,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.jdbc.AutoConfigureDataJdbc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 
 @SpringBootTest
@@ -25,6 +29,13 @@ class AgendaDeConsultasTest {
     @DisplayName("Deve Devolver Código Http 404 quando informações forem invalidas...")
     void agendar_cenario1() throws Exception {
 
+        var response = mockMvc.perform(
+                post("/consultas")
+        ).andReturn().getResponse();
+
+
+        assertThat(response.getStatus())
+                .isEqualTo(HttpStatus.BAD_REQUEST.value());
 
     }
 }
