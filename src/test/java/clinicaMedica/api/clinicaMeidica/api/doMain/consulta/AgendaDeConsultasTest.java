ced2e307262c949221d8702e8c591efb154e2b39
jadeilton <jadeiltondm@gmail.com>
2024-01-01 11:17:26 -0300

    feat: add library MockMvc

--- src/test/java/clinicaMedica/api/clinicaMeidica/api/doMain/consulta/AgendaDeConsultasTest.java
+++ src/test/java/clinicaMedica/api/clinicaMeidica/api/doMain/consulta/AgendaDeConsultasTest.java
@@ -1,7 +1,22 @@
 package clinicaMedica.api.clinicaMeidica.api.doMain.consulta;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.jdbc.AutoConfigureDataJdbc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+
+@SpringBootTest
+@AutoConfigureDataJdbc
+@WithMockUser
 class AgendaDeConsultasTest {
+
+
+
+    @Autowired
+    private MockMvc mockMvc;
 
 }
