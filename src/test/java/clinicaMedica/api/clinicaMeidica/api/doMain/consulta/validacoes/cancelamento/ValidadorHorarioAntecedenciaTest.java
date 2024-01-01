bb4bf22e18e76f1764768772b6297082be8972d6
jadeilton <jadeiltondm@gmail.com>
2024-01-01 11:03:10 -0300

    feat: create class test ValidadorHorarioAntecedenciaTest

---
+++ src/test/java/clinicaMedica/api/clinicaMeidica/api/doMain/consulta/validacoes/cancelamento/ValidadorHorarioAntecedenciaTest.java
@@ -0,0 +1,7 @@
+package clinicaMedica.api.clinicaMeidica.api.doMain.consulta.validacoes.cancelamento;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ValidadorHorarioAntecedenciaTest {
+
+}
