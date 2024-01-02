cd622512ca60826265b6d899ed58336f6fdcf33c
jadeilton <jadeiltondm@gmail.com>
2024-01-02 09:51:25 -0300

    feat: method agendar_cenario1 in class AgendaDeConsultasTest, tested.

--- src/test/java/clinicaMedica/api/clinicaMeidica/api/doMain/consulta/AgendaDeConsultasTest.java
+++ src/test/java/clinicaMedica/api/clinicaMeidica/api/doMain/consulta/AgendaDeConsultasTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.jdbc.AutoConfigureDataJdbc;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.test.context.support.WithMockUser;
@@ -16,7 +17,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 
 
 @SpringBootTest
-@AutoConfigureDataJdbc
+@AutoConfigureMockMvc
 @WithMockUser
 class AgendaDeConsultasTest {
 
