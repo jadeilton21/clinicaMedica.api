1291fe1064e0e5c6cb94ddd00c7f56905e256077
jadeilton <jadeiltondm@gmail.com>
2024-01-02 20:53:45 -0300

    feat: create test for method atualizarInformacoes.

--- src/test/java/clinicaMedica/api/clinicaMeidica/api/doMain/enfermeiro/EnfermeiroTest.java
+++ src/test/java/clinicaMedica/api/clinicaMeidica/api/doMain/enfermeiro/EnfermeiroTest.java
@@ -1,5 +1,6 @@
 package clinicaMedica.api.clinicaMeidica.api.doMain.enfermeiro;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,6 +19,7 @@ class EnfermeiroTest {
     private MockMvc mockMvc;
 
 
-
-
+    @Test
+    void atualizarInformacoes() {
+    }
 }
