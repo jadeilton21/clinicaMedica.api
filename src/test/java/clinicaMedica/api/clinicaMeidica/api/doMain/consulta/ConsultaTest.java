276f40be602255a0a4a51c1b90fe68592e44dc4c
jadeilton <jadeiltondm@gmail.com>
2024-01-02 09:58:04 -0300

    feat: create class ConsultaTest

--- src/test/java/clinicaMedica/api/clinicaMeidica/api/doMain/consulta/ConsultaTest.java
+++ src/test/java/clinicaMedica/api/clinicaMeidica/api/doMain/consulta/ConsultaTest.java
@@ -1,7 +1,21 @@
 package clinicaMedica.api.clinicaMeidica.api.doMain.consulta;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@WithMockUser
 class ConsultaTest {
 
+
+    @Autowired
+    private MockMvc mockMvc;
 }
