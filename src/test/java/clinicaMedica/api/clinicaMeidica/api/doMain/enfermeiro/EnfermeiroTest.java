da7a26b870226b2b3b4f17cb1ff270da0ff5c619
jadeilton <jadeiltondm@gmail.com>
2024-01-02 20:56:24 -0300

    feat: create arguments in method atualizarInformacoes_cenario1.

--- src/test/java/clinicaMedica/api/clinicaMeidica/api/doMain/enfermeiro/EnfermeiroTest.java
+++ src/test/java/clinicaMedica/api/clinicaMeidica/api/doMain/enfermeiro/EnfermeiroTest.java
@@ -5,9 +5,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
@@ -32,8 +34,8 @@ class EnfermeiroTest {
 
 
 
-
-
+        assertThat(response.getStatus())
+                .isEqualTo(HttpStatus.BAD_REQUEST.value());
 
     }
 }
