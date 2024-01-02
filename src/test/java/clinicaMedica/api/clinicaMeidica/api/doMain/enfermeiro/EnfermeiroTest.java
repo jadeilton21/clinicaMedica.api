092aa5c73a240d11a7394cfb253096c776383d89
jadeilton <jadeiltondm@gmail.com>
2024-01-02 20:52:48 -0300

    feat: add libary MockMvc for tests

--- src/test/java/clinicaMedica/api/clinicaMeidica/api/doMain/enfermeiro/EnfermeiroTest.java
+++ src/test/java/clinicaMedica/api/clinicaMeidica/api/doMain/enfermeiro/EnfermeiroTest.java
@@ -1,7 +1,20 @@
 package clinicaMedica.api.clinicaMeidica.api.doMain.enfermeiro;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+@AutoConfigureMockMvc
+@WithMockUser
 class EnfermeiroTest {
+
+
+
+    @Autowired
+    private MockMvc mockMvc;
 
 }
