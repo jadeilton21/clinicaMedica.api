18ff62b7d0ce61d5b95d01c9513a4eaa9e92fe3d
jadeilton <jadeiltondm@gmail.com>
2024-01-02 10:03:20 -0300

    feat: create arguments in test cancelar_cenario1.

--- src/test/java/clinicaMedica/api/clinicaMeidica/api/doMain/consulta/ConsultaTest.java
+++ src/test/java/clinicaMedica/api/clinicaMeidica/api/doMain/consulta/ConsultaTest.java
@@ -5,11 +5,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
-
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 
 @SpringBootTest
@@ -24,5 +26,12 @@ class ConsultaTest {
     @Test
     @DisplayName("Deve Devolver Código Http 404 quando informações forem invalidas..")
     void cancelar_cenario1() throws Exception{
+
+        var respones = mockMvc
+                .perform(post("/consultas"))
+                .andReturn().getResponse();
+
+        assertThat(respones.getStatus())
+                .isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 }
