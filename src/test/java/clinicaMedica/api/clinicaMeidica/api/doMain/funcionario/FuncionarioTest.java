7e956db1d5be914860708721d81b56e5af4030ee
jadeilton <jadeiltondm@gmail.com>
2024-01-02 21:06:14 -0300

    feat: create method atualizarInformacoes

--- src/test/java/clinicaMedica/api/clinicaMeidica/api/doMain/funcionario/FuncionarioTest.java
+++ src/test/java/clinicaMedica/api/clinicaMeidica/api/doMain/funcionario/FuncionarioTest.java
@@ -1,7 +1,12 @@
 package clinicaMedica.api.clinicaMeidica.api.doMain.funcionario;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FuncionarioTest {
 
+    @Test
+    void atualizarInformacoes() {
+    }
 }
