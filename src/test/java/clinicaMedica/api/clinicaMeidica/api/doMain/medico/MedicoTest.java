d83bcc81a69a7dc6f09a5a3696d64c73e403f813
jadeilton <jadeiltondm@gmail.com>
2024-01-03 09:53:12 -0300

    feat:create class test MedicoTest

---
+++ src/test/java/clinicaMedica/api/clinicaMeidica/api/doMain/medico/MedicoTest.java
@@ -0,0 +1,12 @@
+package clinicaMedica.api.clinicaMeidica.api.doMain.medico;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MedicoTest {
+
+    @Test
+    void atualizarInformacoes() {
+    }
+}
