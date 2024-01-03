5391e80935aa793362369be97c462adc43e6e8fa
jadeilton <jadeiltondm@gmail.com>
2024-01-03 09:54:20 -0300

    feat:create test atualizarInformacoes_cenario1.

--- src/test/java/clinicaMedica/api/clinicaMeidica/api/doMain/medico/MedicoTest.java
+++ src/test/java/clinicaMedica/api/clinicaMeidica/api/doMain/medico/MedicoTest.java
@@ -1,12 +1,23 @@
 package clinicaMedica.api.clinicaMeidica.api.doMain.medico;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.jdbc.AutoConfigureDataJdbc;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@SpringBootTest
+@AutoConfigureMockMvc
+@WithMockUser
 class MedicoTest {
 
+
+    @Autowired
+    private MockMvc mockMvc;
     @Test
-    void atualizarInformacoes() {
+    void atualizarInformacoes_cenario1() throws Exception{
     }
 }
