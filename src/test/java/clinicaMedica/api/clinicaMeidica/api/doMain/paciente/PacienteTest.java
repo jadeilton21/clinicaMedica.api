5d5afda76de0b65abb50c7597d45517f2f883dfd
jadeilton <jadeiltondm@gmail.com>
2024-01-03 11:11:05 -0300

    feat: create arguments in method atualizarInformacoes_cenario1.

--- src/test/java/clinicaMedica/api/clinicaMeidica/api/doMain/paciente/PacienteTest.java
+++ src/test/java/clinicaMedica/api/clinicaMeidica/api/doMain/paciente/PacienteTest.java
@@ -5,10 +5,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
 @SpringBootTest
 @AutoConfigureMockMvc
 @WithMockUser
@@ -23,6 +27,11 @@ class PacienteTest {
     @DisplayName("Deve Devolver Código Http 404 quando informações forem invalidas..")
     void atualizarInfomacoes_cenario1() throws Exception {
 
+        var response = mockMvc
+                .perform(post("pacientes"))
+                .andReturn().getResponse();
 
+        assertThat(response.getStatus())
+                .isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 }
